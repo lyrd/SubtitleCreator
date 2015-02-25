c05187b679c0334edb7eddf3cdd84c8da212ce7b
lyrd <antonmol-92@yandex.ru>
2015-02-25 17:11:18 +0800

    v.0.0.0.2
    Добавлено извлечение звуковой дорожки

---
+++ MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SubtitleCreator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.tBInputVideo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 318);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(103, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "test_Получить дорожку";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // tBInputVideo
+            // 
+            this.tBInputVideo.Location = new System.Drawing.Point(6, 19);
+            this.tBInputVideo.Name = "tBInputVideo";
+            this.tBInputVideo.Size = new System.Drawing.Size(535, 20);
+            this.tBInputVideo.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.tBInputVideo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(633, 50);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Исходный видеофайл";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(547, 17);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Обзор";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 353);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox tBInputVideo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubtitleCreator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /*
+             * TODO:
+             * block form while cmd
+             */
+
+        /*
+         * ffmpeg keys
+         * ffmpeg -i source_video.avi -vn -ar 44100 -ac 2 -ab 192k -f mp3 sound.mp3
+         * --------------------------------------------------------------------------------------------------------------------------------
+         * Ключ 	      |  Пример      |	Описание 
+         * --------------------------------------------------------------------------------------------------------------------------------
+         * -i <filename>  |	-i movie.avi |	Путь/имя входного файла. Без установки параметров обработки означает информацию о входном файле 
+         * -vn 	          | -vn 	     |  Не кодировать видео 
+         * -ar <freq> 	  | -ar 48000    |  Частота дискредитации. По умочанию 44100Hz 
+         * -ac <channels> | -aс 6        |  Количество каналов. По умочанию 2
+         * -ab <bitrate>  | -ab 256k 	 |  Битрейт аудио. По умочанию 64kbit/s 
+         * -f <format> 	  | -f mkv       |	Формат входного/выходного файла 
+         */
+
+        private string inputVideoFile = "";
+        private string outputAudioFile = "";
+        private string name = ""; 
+        private string format = "wav";
+
+        private string Transliteration(string source)
+        {
+            #region Letters
+            Dictionary<string, string> letters = new Dictionary<string, string>();
+            letters.Add("а", "a");
+            letters.Add("б", "b");
+            letters.Add("в", "v");
+            letters.Add("г", "g");
+            letters.Add("д", "d");
+            letters.Add("е", "e");
+            letters.Add("ё", "yo");
+            letters.Add("ж", "zh");
+            letters.Add("з", "z");
+            letters.Add("и", "i");
+            letters.Add("й", "j");
+            letters.Add("к", "k");
+            letters.Add("л", "l");
+            letters.Add("м", "m");
+            letters.Add("н", "n");
+            letters.Add("о", "o");
+            letters.Add("п", "p");
+            letters.Add("р", "r");
+            letters.Add("с", "s");
+            letters.Add("т", "t");
+            letters.Add("у", "u");
+            letters.Add("ф", "f");
+            letters.Add("х", "h");
+            letters.Add("ц", "c");
+            letters.Add("ч", "ch");
+            letters.Add("ш", "sh");
+            letters.Add("щ", "sch");
+            letters.Add("ъ", "j");
+            letters.Add("ы", "i");
+            letters.Add("ь", "j");
+            letters.Add("э", "e");
+            letters.Add("ю", "yu");
+            letters.Add("я", "ya");
+            letters.Add("А", "A");
+            letters.Add("Б", "B");
+            letters.Add("В", "V");
+            letters.Add("Г", "G");
+            letters.Add("Д", "D");
+            letters.Add("Е", "E");
+            letters.Add("Ё", "Yo");
+            letters.Add("Ж", "Zh");
+            letters.Add("З", "Z");
+            letters.Add("И", "I");
+            letters.Add("Й", "J");
+            letters.Add("К", "K");
+            letters.Add("Л", "L");
+            letters.Add("М", "M");
+            letters.Add("Н", "N");
+            letters.Add("О", "O");
+            letters.Add("П", "P");
+            letters.Add("Р", "R");
+            letters.Add("С", "S");
+            letters.Add("Т", "T");
+            letters.Add("У", "U");
+            letters.Add("Ф", "F");
+            letters.Add("Х", "H");
+            letters.Add("Ц", "C");
+            letters.Add("Ч", "Ch");
+            letters.Add("Ш", "Sh");
+            letters.Add("Щ", "Sch");
+            letters.Add("Ъ", "J");
+            letters.Add("Ы", "I");
+            letters.Add("Ь", "J");
+            letters.Add("Э", "E");
+            letters.Add("Ю", "Yu");
+            letters.Add("Я", "Ya");
+            #endregion
+
+            foreach (KeyValuePair<string, string> pair in letters)
+            {
+                source = source.Replace(pair.Key, pair.Value);
+            }
+
+            return source;
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {       
+            System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + "\\ffmpeg.exe", String.Format(" -i \"{0}\" -vn -ar 44100 -ac 2 -ab 192k -f {1} {2}", inputVideoFile, format, outputAudioFile));
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                inputVideoFile = dialog.FileName;
+            }
+            tBInputVideo.Text = inputVideoFile;
+
+            name = Transliteration(System.IO.Path.GetFileNameWithoutExtension(inputVideoFile)).Replace(' ','_');
+            outputAudioFile = String.Format("{0}\\{1}.{2}", Directory.GetCurrentDirectory(), name, format);
+        }
+    }
+}
