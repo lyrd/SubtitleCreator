0edee3195cf9728e54eb7dba802f7c50ed96f6d2
lyrd <antonmol-92@yandex.ru>
2015-05-26 15:44:57 +0800

    v.0.0.0.21
    + Read from mfcc base

--- Classes/AudioProcessor.cs
+++ Classes/AudioProcessor.cs
@@ -22,6 +22,8 @@ namespace SubtitleCreator
         private static List<int> pointsOfChangeValues = new List<int>();
         private static List<Point> borders = new List<Point>();
 
+        private static readonly string pathToBase = /*Directory.GetCurrentDirectory() +*/ "mfccBase.mfcc";
+
         private static List<Frame> Frames
         {
             get { return combinedFrames; }
@@ -29,8 +31,6 @@ namespace SubtitleCreator
 
         private static string GetTime(uint size)
         {
-            //double time = size / (44100d * 1d * (16d / 8d));
-            //return String.Format("{0}:{1}:{2}", 00, 00, Math.Round(time, 3));
             TimeSpan interval = TimeSpan.FromSeconds(size / (44100d * 1d * (16d / 8d)));
             return String.Format("{0}:{1}:{2},{3}", interval.Hours, interval.Minutes, interval.Seconds, interval.Milliseconds);
         }
@@ -150,83 +150,37 @@ namespace SubtitleCreator
             });
         }
 
-        //TODO: LoadMFCC from file
+        private static void ReadFromDataBase(ref Dictionary<string, double[]> samplesMFCC, string pathToBase)
+        {
+            using (StreamReader streamReader = new StreamReader(pathToBase, Encoding.UTF8))
+            {
+                while (true)
+                {
+                    string[] line = new string[2];
+                    string[] array = new string[12];
+                    double[] mfccs = new double[12];
+
+                    string temp = streamReader.ReadLine();
+
+                    if (temp == null) break;
+
+                    line = temp.Split(';');
+                    array = line[1].Split('/');
+
+                    for (int i = 0; i < array.Length; i++)
+                        Double.TryParse(array[i], out mfccs[i]);
+
+                    samplesMFCC.Add(line[0], mfccs);
+                }
+            }
+        }
+
         private static void GetCaption()
         {
             Dictionary<string, double[]> samplesMFCC = new Dictionary<string, double[]>();
+          
+            ReadFromDataBase(ref samplesMFCC, pathToBase);
 
-            #region 1-5
-            samplesMFCC.Add("Один", new double[] {115.902755904026,
-26.4175781482317,
--19.4969240820016,
-15.9409905913534,
--10.4284405539948,
-2.96120634212882,
-0.839603731203302,
--3.54910751739161,
-7.03148074595154,
--3.38572657631652,
-4.31563689768913,
--1.83372751201521
-});
-
-            samplesMFCC.Add("Два", new double[] { 87.4680793373385,
-38.04600031747,
--13.1417323331799,
-20.2925001019083,
--5.30224524545408,
-7.91420814520266,
-1.30036853260372,
--5.17687413666072,
-4.37913810603501,
--6.49839167365708,
-1.62132894134803,
--3.20190955072429
-            });
-
-            samplesMFCC.Add("Три", new double[] { 90.4941289080472,
-26.0453417615593,
--18.6743363735187,
-1.74459319558932,
--8.92167293158036,
-2.24214961688164,
-1.87286420970423,
--3.12817588006035,
-4.87943552319365,
-0.372040941839539,
-1.6383080532519,
--1.84132489151332
-            });
-
-            samplesMFCC.Add("Четыре", new double[] { 105.925500830823,
-23.8592925836023,
--22.2180238851642,
-9.02482389959434,
--9.97740789791419,
-1.1395326330837,
-2.70388567208278,
--1.595824974041,
-6.23204003075054,
--1.41871022985522,
-2.82935206354146,
--1.50896863327325
-            });
-
-            samplesMFCC.Add("Пять", new double[] { 104.783221920618,
-41.9621287055811,
--13.6065046872223,
-11.7495013975808,
--10.9850334035548,
-3.68997271801716,
-2.65331674949439,
--3.00099818531845,
-4.97058029287006,
--1.78037702637737,
-3.76850879675914,
--1.85753593805106
-            });
-            #endregion
-            
             List<double> tempMinDistances = new List<double>();
 
             for (int j = 0; j < AudioProcessor.Frames.Count; j++)
